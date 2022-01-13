6eda919ac5daea2d9d80ff609333f682ebcf1bdb
Charles Desiderio <cjdesi@gmail.com>
2022-01-12 21:41:21 -0600

    Playing with Svelte

---
+++ src/routes/component.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let item  
+
+  const toggleStatus = () => {
+    console.log(item)
+  }
+  
+</script>
+
+<li on:click={toggleStatus}>{item}</li>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let list = []
+  let inputValue = ''
+
+  const addToList = () => {
+    list = [...list, inputValue]
+    inputValue = ''
+    console.log(list)
+  }
+
+  import Component from "./component.svelte";
+
+</script>
+
+<div>
+<h1>To Do List</h1>
+<input bind:value={inputValue}>
+<button on:click={addToList}>Submit</button>
+{#each list as listItem}
+<Component item={listItem} />
+{/each}
+
+
+</div>
